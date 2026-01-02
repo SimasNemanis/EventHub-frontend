5bdf4cd7ec734d874fb597af47217b5246a0aed0
SimasNemanis <114734045+SimasNemanis@users.noreply.github.com>
2026-01-02 02:23:37 +0200

    Delete src/api/base44Client.js

--- src/api/base44Client.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@base44/sdk';
-// import { getAccessToken } from '@base44/sdk/utils/auth-utils';
-
-// Create a client with authentication required
-export const base44 = createClient({
-  appId: "690c4af43a801821e2826b57", 
-  requiresAuth: true // Ensure authentication is required for all operations
-});
